02dc19dd67371d0b2f6bac523390b6632bee029c
talyz <kim.lindberger@gmail.com>
2020-06-07 14:36:10 +0200

    home-manager: Bind mount directories instead of symlinking them
    Use bindfs to create bind mounts for directories instead of symlinking
    them. This should be less problematic for many applications, since
    bind mounts are much more transparent.

--- home-manager.nix
+++ home-manager.nix
@@ -6,7 +6,7 @@ let
 
   persistentStoragePaths = attrNames cfg;
 
-  inherit (pkgs.callPackage ./lib.nix { }) splitPath dirListToPath concatPaths;
+  inherit (pkgs.callPackage ./lib.nix { }) splitPath dirListToPath concatPaths sanitizeName;
 in
 {
   options = {
@@ -43,53 +43,171 @@ in
       let
         link = file:
           pkgs.runCommand
-            "${replaceStrings [ "/" "." " " ] [ "-" "" "" ] file}"
+            "${sanitizeName file}"
             { }
             "ln -s '${file}' $out";
 
-        mkLinkNameValuePair = persistentStoragePath: fileOrDir: {
+        mkLinkNameValuePair = persistentStoragePath: file: {
           name =
             if cfg.${persistentStoragePath}.removePrefixDirectory then
-              dirListToPath (tail (splitPath [ fileOrDir ]))
+              dirListToPath (tail (splitPath [ file ]))
             else
-              fileOrDir;
-          value = { source = link (concatPaths [ persistentStoragePath fileOrDir ]); };
+              file;
+          value = { source = link (concatPaths [ persistentStoragePath file ]); };
         };
 
         mkLinksToPersistentStorage = persistentStoragePath:
           listToAttrs (map
             (mkLinkNameValuePair persistentStoragePath)
-            (cfg.${persistentStoragePath}.files ++ cfg.${persistentStoragePath}.directories)
+            (cfg.${persistentStoragePath}.files)
           );
       in
       foldl' recursiveUpdate { } (map mkLinksToPersistentStorage persistentStoragePaths);
+
+    systemd.user.services =
+      let
+        mkBindMountService = persistentStoragePath: dir:
+          let
+            mountDir =
+              if cfg.${persistentStoragePath}.removePrefixDirectory then
+                dirListToPath (tail (splitPath [ dir ]))
+              else
+                dir;
+            targetDir = concatPaths [ persistentStoragePath dir ];
+            mountPoint = concatPaths [ config.home.homeDirectory mountDir ];
+            name = "bindMount-${sanitizeName targetDir}";
+            startScript = pkgs.writeShellScript name ''
+              set -eu
+              if ! mount | grep "${mountPoint}"; then
+                  ${pkgs.bindfs}/bin/bindfs -f --no-allow-other "${targetDir}" "${mountPoint}"
+              else
+                  echo "There is already an active mount at or below ${mountPoint}!" >&2
+                  exit 1
+              fi
+            '';
+            stopScript = pkgs.writeShellScript "unmount-${name}" ''
+              fusermount -uz "${mountPoint}"
+            '';
+          in
+          {
+            inherit name;
+            value = {
+              Unit = {
+                Description = "Bind mount ${targetDir} at ${mountPoint}";
+                PartOf = [ "graphical-session-pre.target" ];
+
+                # Don't restart the unit, it could corrupt data and
+                # crash programs currently reading from the mount.
+                X-RestartIfChanged = false;
+              };
+
+              Install.WantedBy = [ "default.target" ];
+
+              Service = {
+                ExecStart = "${startScript}";
+                ExecStop = "${stopScript}";
+              };
+            };
+          };
+
+        mkBindMountServicesForPath = persistentStoragePath:
+          listToAttrs (map
+            (mkBindMountService persistentStoragePath)
+            cfg.${persistentStoragePath}.directories
+          );
+      in
+      builtins.foldl'
+        recursiveUpdate
+        { }
+        (map mkBindMountServicesForPath persistentStoragePaths);
 
     home.activation =
       let
         dag = config.lib.dag;
 
-        mkDirCreationSnippet = persistentStoragePath: dir:
+        declareMountedPathsArray = {
+          name = "declareMountedPathsArray";
+          value =
+            dag.entryBefore
+              [ "writeBoundary" ]
+              "declare -A mountedPaths";
+        };
+
+        mkBindMountSnippet = persistentStoragePath: dir:
           let
+            mountDir =
+              if cfg.${persistentStoragePath}.removePrefixDirectory then
+                dirListToPath (tail (splitPath [ dir ]))
+              else
+                dir;
             targetDir = concatPaths [ persistentStoragePath dir ];
+            mountPoint = concatPaths [ config.home.homeDirectory mountDir ];
+            systemctl = "XDG_RUNTIME_DIR=\${XDG_RUNTIME_DIR:-/run/user/$(id -u)} ${config.systemd.user.systemctlPath}";
           in
           ''
             if [[ ! -e "${targetDir}" ]]; then
                 mkdir -p "${targetDir}"
             fi
+            if [[ ! -e "${mountPoint}" ]]; then
+                mkdir -p "${mountPoint}"
+            fi
+            if mount | grep "${mountPoint}"; then
+                if mount | grep "${mountPoint}" | grep "${targetDir}"; then
+                    mountedPaths["${mountPoint}"]=0
+                else
+                    # The target directory changed, so we need to remount
+                    echo "remounting ${mountPoint}"
+                    ${systemctl} --user stop bindMount-${sanitizeName targetDir}
+                    ${pkgs.bindfs}/bin/bindfs --no-allow-other "${targetDir}" "${mountPoint}"
+                    mountedPaths["${mountPoint}"]=1
+                fi
+            else
+                ${pkgs.bindfs}/bin/bindfs --no-allow-other "${targetDir}" "${mountPoint}"
+                mountedPaths["${mountPoint}"]=1
+            fi
           '';
 
-        mkDirCreationScriptForPath = persistentStoragePath: {
-          name = "createDirsIn-${replaceStrings [ "/" "." " " ] [ "-" "" "" ] persistentStoragePath}";
+        mkBindMountScriptForPath = persistentStoragePath: {
+          name = "createAndMount-" + (sanitizeName persistentStoragePath);
           value =
             dag.entryAfter
               [ "writeBoundary" ]
               (concatMapStrings
-                (mkDirCreationSnippet persistentStoragePath)
+                (mkBindMountSnippet persistentStoragePath)
                 cfg.${persistentStoragePath}.directories
               );
         };
+
+        mkUnmountSnippet = persistentStoragePath: dir:
+          let
+            mountDir =
+              if cfg.${persistentStoragePath}.removePrefixDirectory then
+                dirListToPath (tail (splitPath [ dir ]))
+              else
+                dir;
+            mountPoint = concatPaths [ config.home.homeDirectory mountDir ];
+          in
+          ''
+            if [[ ''${mountedPaths["${mountPoint}"]} == 1 ]]; then
+                fusermount -u "${mountPoint}"
+            fi
+          '';
+
+        mkUnmountScriptForPath = persistentStoragePath: {
+          name = "unmount-" + (sanitizeName persistentStoragePath);
+          value =
+            dag.entryBefore
+              [ "reloadSystemD" ]
+              (concatMapStrings
+                (mkUnmountSnippet persistentStoragePath)
+                cfg.${persistentStoragePath}.directories
+              );
+        };
+
       in
-      listToAttrs (map mkDirCreationScriptForPath persistentStoragePaths);
+      listToAttrs ([ declareMountedPathsArray ]
+        ++ (map mkBindMountScriptForPath persistentStoragePaths)
+        ++ (map mkUnmountScriptForPath persistentStoragePaths));
   };
 
 }
